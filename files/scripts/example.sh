f843f23f1fa6e831837f9e28b514d86a3c89d337
Stoyan Georgiev <93016278+st0y4n@users.noreply.github.com>
2025-12-30 22:12:13 +0200

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,3 +8,11 @@ set -oue pipefail
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
+
+COPY --from=ghcr.io/ublue-os/akmods-nvidia-lts:latest / /tmp/akmods-nvidia
+RUN find /tmp/akmods-nvidia
+## optionally install remove old and install new kernel
+# dnf -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+## install ublue support package and desired kmod(s)
+RUN dnf install /tmp/rpms/ublue-os/ublue-os-nvidia*.rpm
+RUN dnf install /tmp/rpms/kmods/kmod-nvidia*.rpm
